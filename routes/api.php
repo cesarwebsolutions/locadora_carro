f31f770a7c0221397573e5575a4196c98baec605
César Henrique de Figueiredo <cesar.sprite@gmail.com>
2022-01-22 17:58:20 -0300

    rotas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
+Route::apiResource('carro', 'App\Http\Controllers\CarroController');
+Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
+Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
+Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
